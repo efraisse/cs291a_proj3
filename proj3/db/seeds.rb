52aa5a1e1f6e7f9cc75f62c4a99a66a05b6ffc6d
Rhys <rhystracy@ucsb.edu>
2022-10-17 22:18:25 -0700

    rt - fixed error with seeds that prevented deletion of db seeds

--- proj3/db/seeds.rb
+++ proj3/db/seeds.rb
@@ -4,28 +4,9 @@
 
 users = User.create([
     {
-        iduser: 11,
-        nickname: "bob"
-    },
-    {
-        iduser: 12,
-        nickname: "bob2"
-    },
-    {
-        iduser: 13,
-        nickname: "bob3"
-    },
-    {
-        iduser: 14,
-        nickname: "bob4"
-    },
-    {
-        iduser: 15,
-        nickname: "bob5"
-    },
-    {
-        iduser: 16,
-        nickname: "bob6"
+        iduser: 21,
+        id: 21,
+        nickname: "user21"
     }
 ])
 
@@ -34,40 +15,12 @@ p "Created #{User.count} users"
 
 posts = Post.create([
     {
-        idpost: 31,
-        text: "nice post 1!",
-        imageurl: "imageurl1",
+        idpost: 11,
+        id: 11,
+        text: "post 11 text, nice!",
+        imageurl: "imageurl11",
+        user_id: 21,
         user: users.first
-    },
-    {
-        idpost: 32,
-        text: "nice post 2!",
-        imageurl: "imageurl2",
-        user: users.second
-    },
-    {
-        idpost: 33,
-        text: "nice post 3!",
-        imageurl: "imageurl3",
-        user: users.third
-    },
-    {
-        idpost: 34,
-        text: "nice post 4!",
-        imageurl: "imageurl4",
-        user: users.fourth
-    },
-    {
-        idpost: 35,
-        text: "nice post 5!",
-        imageurl: "imageurl5",
-        user: users.fifth
-    },
-    {
-        idpost: 36,
-        text: "nice post 6!",
-        imageurl: "imageurl6",
-        user: users[5]
     }
 ])
 
@@ -75,41 +28,14 @@ p "Created #{Post.count} posts"
 
 comments = Comment.create([
     {
-        idcomment: 21,
-        text: "nice post 1!",
+        idcomment: 1,
+        id: 1,
+        text: "comment 1!",
+        user_id: 21,
+        post_id: 11,
         post: posts.first,
         user: users.first
-    },
-    {
-        idcomment: 22,
-        text: "nice post 2!",
-        post: posts.second,
-        user: users.second
-    },
-    {
-        idcomment: 23,
-        text: "nice post 3!",
-        post: posts.third,
-        user: users.third
-    },
-    {
-        idcomment: 24,
-        text: "nice post 4!",
-        post: posts.fourth,
-        user: users.fourth
-    },
-    {
-        idcomment: 25,
-        text: "nice post 5!",
-        post: posts.fifth,
-        user: users.fifth
-    },
-    {
-        idcomment: 26,
-        text: "nice post 6!",
-        post: posts[5],
-        user: users[5]
-    }   
+    } 
 ])
 
 p "Created #{Comment.count} comments"
